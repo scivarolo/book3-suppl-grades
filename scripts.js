12c9e4f50bb6c71b6ada14c516cceba886a8d2d0
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-20 17:20:51 -0500

    Add starter code

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+const grades = {} // You'll need to change this line of code
+
+for (let i = 0; i < scores.length; i++) {
+  /*
+    If the score is greater than 90, increment grades.A by 1.
+
+    You can use a series of `if/then` blocks, but you could
+    also achieve this with a switch statement.
+  */
+
+  
+
+}
